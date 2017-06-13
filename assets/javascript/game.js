147c5680f2d5a853bf7b8cc13bfad1da39a84278
Wes Van Duine <Wes Van Duine>
2017-06-13 11:32:28 -0700

    html and initial variables in JS

---
+++ assets/javascript/game.js
@@ -0,0 +1,24 @@
+/////////////////////////////////////////////////////////////////////
+//                            Variables                            //
+/////////////////////////////////////////////////////////////////////
+
+/*letters to be chosen for game*/
+var letters = ["a", "b", "c"];
+
+/*Array to hold the letters guessed*/
+var guessedLetters = [];
+
+/* This variable will be randomly assigned one of the three letters*/
+var letterToGuess = null;
+
+/*use to countdown*/
+var guessesLeft = 9;
+
+/*Counter for wins and losses*/
+var wins = 0;
+var losses= 0;
+
+/////////////////////////////////////////////////////////////////////
+//                            Functions                            //
+/////////////////////////////////////////////////////////////////////
+
